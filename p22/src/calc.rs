dba34b53af86fa1dfd7f56603ae7d57fa06389ae
unknown <ritam.bhaumik@tii.ae>
2024-06-04 14:07:24 +0400

    Modify module calc

--- p22/src/calc.rs
+++ p22/src/calc.rs
@@ -34,16 +34,11 @@ pub fn fibonacci_loop(n: u32) -> u64 {
     if n == 0 {
         return 0;
     }
-    let mut i: u32 = 1;
     let mut first: u64 = 0;
     let mut second: u64 = 1;
-    loop {
-        if i >= n {
-            break;
-        }
+    for _ in 1..n {
         second += first;
         first = second - first;
-        i += 1;
     }
     second
 }
@@ -61,5 +56,41 @@ pub fn fibonacci_rec(n: u32) -> u64 {
         0 => 0,
         1 => 1,
         m => fibonacci_rec(m - 1) + fibonacci_rec(m - 2),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::celsius2farenheit;
+    use super::farenheit2celsius;
+    use super::fibonacci_loop;
+    use super::fibonacci_rec;
+
+    #[test]
+    fn test_celsius2farenheit() {
+        assert_eq!(celsius2farenheit(5), 41);
+        assert_eq!(celsius2farenheit(-10), 14);
+        assert_eq!(celsius2farenheit(-100), -148);
+    }
+
+    #[test]
+    fn test_farneheit2celsius() {
+        assert_eq!(farenheit2celsius(23), -5);
+        assert_eq!(farenheit2celsius(-13), -25);
+        assert_eq!(farenheit2celsius(392), 200);
+    }
+
+    #[test]
+    fn test_fibonacci_loop() {
+        assert_eq!(fibonacci_loop(4), 3);
+        assert_eq!(fibonacci_loop(9), 34);
+        assert_eq!(fibonacci_loop(13), 233);
+    }
+
+    #[test]
+    fn test_fibonacci_rec() {
+        assert_eq!(fibonacci_rec(6), 8);
+        assert_eq!(fibonacci_rec(7), 13);
+        assert_eq!(fibonacci_rec(12), 144);
     }
 }
