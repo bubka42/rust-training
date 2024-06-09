aadbc25fb3fdd4f296f0acee9aa4578d4c176124
unknown <ritam.bhaumik@tii.ae>
2024-06-09 23:41:04 +0400

    Corrections to p24

--- p24/src/refs.rs
+++ p24/src/refs.rs
@@ -53,14 +53,14 @@ pub fn f3(slice: &mut [u32], n: usize) -> &mut u32 {
 /// let mut slice = [5, 10, 12, 18, 32];
 /// assert_eq!(f4(&mut slice).3[0], 18);
 /// ```
-pub fn f4(slice: &mut [u32]) -> (&[u32], &[u32], &[u32], &[u32]) {
+pub fn f4(slice: &[u32]) -> (&[u32], &[u32], &[u32], &[u32]) {
     let l = slice.len();
     let (a, b, c) = match l % 4 {
-        0 => (l / 4, l / 4, l / 4),
+        0 => (l / 4, l / 2, 3 * l / 4),
         1 => ((l - 1) / 4, (l - 1) / 2, 3 * (l - 1) / 4),
         2 => ((l - 2) / 4, l / 2 - 1, 3 * (l - 2) / 4 + 1),
         3 => ((l - 3) / 4, (l - 3) / 2 + 1, 3 * (l - 3) / 4 + 2),
-        _ => panic!(),
+        _ => unreachable!(),
     };
     (&slice[..a], &slice[a..b], &slice[b..c], &slice[c..])
 }
@@ -98,7 +98,7 @@ mod test {
     }
 
     #[test]
-    fn f4_test() {
+    fn f4_test1() {
         let mut vec = Vec::from_iter(0..43);
         let slices = f4(vec.as_mut_slice());
         assert_eq!(slices.0.len(), 10);
@@ -108,5 +108,15 @@ mod test {
         assert_eq!(slices.1[2], 12);
         assert_eq!(slices.2[10], 31);
         assert_eq!(slices.3[0], 32);
+    }
+
+    #[test]
+    fn f4_test2() {
+        let mut vec = Vec::from_iter(0..8);
+        let slices = f4(vec.as_mut_slice());
+        assert_eq!(slices.0.len(), 2);
+        assert_eq!(slices.1.len(), 2);
+        assert_eq!(slices.2.len(), 2);
+        assert_eq!(slices.3.len(), 2);
     }
 }
