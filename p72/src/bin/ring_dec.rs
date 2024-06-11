ad7e7fc2a696ebdd1f118c6ad32e3222c939440d
unknown <ritam.bhaumik@tii.ae>
2024-06-11 16:31:39 +0400

    Create crate p72 and add binaries for ring encryption and decryption

---
+++ p72/src/bin/ring_dec.rs
@@ -0,0 +1,19 @@
+use base16ct::mixed;
+use p72::decrypt;
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let input_path = &args[1][..];
+    let output_path = &args[2][..];
+    let key = &args[3];
+
+    let mut buf = [0u8; 16];
+    let key_bytes = mixed::decode(key, &mut buf).unwrap();
+    assert_eq!(key_bytes.len(), 16);
+
+    println!("Decrypting {} into {}...", input_path, output_path);
+    decrypt(input_path, output_path, key_bytes.try_into().unwrap())?;
+    Ok(())
+}
